f915d653e05068c36855ee2bedb1f10e6f394165
JACKAJ16 <zhenya271@gmail.com>
2020-05-12 00:49:46 +0300

    uploading build

---
+++ precache-manifest.8be23efcd0df3ea19b5c0316a8ac41d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812245834b933b5940bf7d3e96e7bea1",
+    "url": "/PixabayApp/index.html"
+  },
+  {
+    "revision": "b0b350c2e977e5f86003",
+    "url": "/PixabayApp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "0c07af70b36beb5c99fb",
+    "url": "/PixabayApp/static/css/main.39c6ad7a.chunk.css"
+  },
+  {
+    "revision": "b0b350c2e977e5f86003",
+    "url": "/PixabayApp/static/js/2.7d6b82dd.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/PixabayApp/static/js/2.7d6b82dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c07af70b36beb5c99fb",
+    "url": "/PixabayApp/static/js/main.1e6a6144.chunk.js"
+  },
+  {
+    "revision": "e37e80d946b06f0e60b5",
+    "url": "/PixabayApp/static/js/runtime-main.985f1b74.js"
+  },
+  {
+    "revision": "5d68548bd7b849847ee1ce27459cc668",
+    "url": "/PixabayApp/static/media/imageedit_3_5890261322.5d68548b.png"
+  }
+]);
